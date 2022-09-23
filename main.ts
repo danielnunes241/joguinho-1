206a8106fd9c2a08403aaddd5f0a9bb985ac3cb1
danielnunes241 <113685705+danielnunes241@users.noreply.github.com>
2022-09-23 08:01:04 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showString("vamos!")
+    basic.showString("vamos jogar")
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
 })
 basic.forever(function () {
 	
