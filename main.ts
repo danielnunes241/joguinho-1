be2f510abb6c81743ddfcfbd75433f0233c18324
danielnunes241 <113685705+danielnunes241@users.noreply.github.com>
2022-09-30 08:00:34 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,27 +1,39 @@
+let tocar = false
+let P_I = 0
+let P_II = 0
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showString("vamos jogar")
+    tocar = false
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
     basic.clearScreen()
+    basic.pause(randint(2000, 5000))
 })
 input.onPinPressed(TouchPin.P2, function () {
-    basic.showLeds(`
-        . . # # #
-        . . # # #
-        . . # # #
-        . . . # #
-        . . . # #
-        `)
+    if (tocar == false) {
+        tocar = true
+        P_I += 1
+        basic.showLeds(`
+            . . # # #
+            . . # # #
+            . . # # #
+            . . . # #
+            . . . # #
+            `)
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showLeds(`
-        # # # . .
-        # # # . .
-        # # # . .
-        # # . . .
-        # # . . .
-        `)
+    if (tocar == false) {
+        tocar = true
+        P_II += 1
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # . . .
+            # # . . .
+            `)
+    }
 })
 basic.forever(function () {
 	
